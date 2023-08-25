567b54d82f126e5c7a9979eceea34331e2a32f22
Daniel Souza de Lima <128085169+DanelLima@users.noreply.github.com>
2023-08-25 20:41:10 -0300

    Add files via upload

--- Variaveis/variaveis.cpp
+++ Variaveis/variaveis.cpp
@@ -1,32 +1,40 @@
 #include<stdio.h>
 #include<locale.h>
+#include<string.h>
 
 int main()
 {
 	setlocale(LC_ALL,"Portuguese");
-	char nome[20],matricula[20],endereco[20],curso[20],disciplina[20];
+	char nome[40],matricula[40],endereco[40],curso[40],disciplina[60];
 	int idade,periodo;
 	
 	printf("Diga seu nome: ");
-	scanf("%s",&nome);
+	fgets(nome, sizeof(nome), stdin);
+    nome[strcspn(nome, "\n")] = '\0';
 	
 	printf("Diga sua matrícula: ");
-	scanf("%s",&matricula);
+	fgets(matricula, sizeof(matricula), stdin);
+    matricula[strcspn(matricula, "\n")] = '\0';
 	
 	printf("Diga seu endereço: ");
-	scanf("%s",&endereco);
+	fgets(endereco, sizeof(endereco), stdin);
+    endereco[strcspn(endereco, "\n")] = '\0';
 	
 	printf("Diga seu curso: ");
-	scanf("%s",&curso);
+	fgets(curso, sizeof(curso), stdin);
+    curso[strcspn(curso, "\n")] = '\0';
 	
 	printf("Diga sua idade: ");
 	scanf("%d",&idade);
+	getchar();
 	
 	printf("Diga seu período: ");
 	scanf("%d",&periodo);
+	getchar();
 	
 	printf("Diga suas disciplinas: ");
-	scanf("%s",&disciplina);
+	fgets(disciplina, sizeof(disciplina), stdin);
+    disciplina[strcspn(disciplina, "\n")] = '\0';
 	
 	printf("%s de %d anos de idade da matrícula %s, você está matriculado no curso %s, na(s) discplina(s) %s, no período %d e seu endereço é %s.",nome,idade,matricula,curso,disciplina,periodo,endereco);
 	return 0;
