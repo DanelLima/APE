d6d6b80adad8bc7ed5d797b49f01976011bb9e3a
Daniel Souza de Lima <128085169+DanelLima@users.noreply.github.com>
2023-09-01 10:12:03 -0300

    Add files via upload

--- Atividade_4/03.c
+++ Atividade_4/03.c
@@ -20,121 +20,121 @@ int main(){
     if (A>=B && A>=C && A>=D){
         printf("O maior número digitado foi %d",A);
         if (B>=C && B>=D){
-            printf("O segundo maior número digitado foi %d",B);
+            printf("\nO segundo maior número digitado foi %d",B);
             if (C>=D){
-                printf("O terceiro maior número digitado foi %d",C);
-                printf("O quarto maior número digitado foi %d",D);
+                printf("\nO terceiro maior número digitado foi %d",C);
+                printf("\nO quarto maior número digitado foi %d",D);
             }else{
-                printf("O terceiro maior número digitado foi %d",D);
-                printf("O quarto maior número digitado foi %d",C);
+                printf("\nO terceiro maior número digitado foi %d",D);
+                printf("\nO quarto maior número digitado foi %d",C);
             }
         } else if (C>=B && C>=D){
-            printf("O segundo maior número digitado foi %d",C);
+            printf("\nO segundo maior número digitado foi %d",C);
             if (B>=D){
-                printf("O terceiro maior número digitado foi %d",B);
-                printf("O quarto maior número digitado foi %d",D);
+                printf("\nO terceiro maior número digitado foi %d",B);
+                printf("\nO quarto maior número digitado foi %d",D);
             }else{
-                printf("O terceiro maior número digitado foi %d",D);
-                printf("O quarto maior número digitado foi %d",B);
+                printf("\nO terceiro maior número digitado foi %d",D);
+                printf("\nO quarto maior número digitado foi %d",B);
             }
         } else{
-            printf("O segundo maior número digitado foi %d",D);
+            printf("\nO segundo maior número digitado foi %d",D);
             if (C>=B){
-                printf("O terceiro maior número digitado foi %d",C);
-                printf("O quarto maior número digitado foi %d",B);
+                printf("\nO terceiro maior número digitado foi %d",C);
+                printf("\nO quarto maior número digitado foi %d",B);
             }else{
-                printf("O terceiro maior número digitado foi %d",B);
-                printf("O quarto maior número digitado foi %d",C);
+                printf("\nO terceiro maior número digitado foi %d",B);
+                printf("\nO quarto maior número digitado foi %d",C);
             }
         }
     } else if (B>=A && B>=C && B>=D){
         printf("O maior número digitado foi %d",B);
         if (A>=C && A>=D){
-            printf("O segundo maior número digitado foi %d",A);
+            printf("\nO segundo maior número digitado foi %d",A);
             if (C>=D){
-                printf("O terceiro maior número digitado foi %d",C);
-                printf("O quarto maior número digitado foi %d",D);
+                printf("\nO terceiro maior número digitado foi %d",C);
+                printf("\nO quarto maior número digitado foi %d",D);
             }else{
-                printf("O terceiro maior número digitado foi %d",D);
-                printf("O quarto maior número digitado foi %d",C);
+                printf("\nO terceiro maior número digitado foi %d",D);
+                printf("\nO quarto maior número digitado foi %d",C);
             }
         } else if (C>=A && C>=D){
-            printf("O segundo maior número digitado foi %d",C);
+            printf("\nO segundo maior número digitado foi %d",C);
             if (A>=D){
-                printf("O terceiro maior número digitado foi %d",A);
-                printf("O quarto maior número digitado foi %d",D);
+                printf("\nO terceiro maior número digitado foi %d",A);
+                printf("\nO quarto maior número digitado foi %d",D);
             }else{
-                printf("O terceiro maior número digitado foi %d",D);
-                printf("O quarto maior número digitado foi %d",A);
+                printf("\nO terceiro maior número digitado foi %d",D);
+                printf("\nO quarto maior número digitado foi %d",A);
             }
         } else{
-            printf("O segundo maior número digitado foi %d",D);
-            if (A>=B){
-                printf("O terceiro maior número digitado foi %d",A);
-                printf("O quarto maior número digitado foi %d",C);
+            printf("\nO segundo maior número digitado foi %d",D);
+            if (A>=C){
+                printf("\nO terceiro maior número digitado foi %d",A);
+                printf("\nO quarto maior número digitado foi %d",C);
             }else{
-                printf("O terceiro maior número digitado foi %d",C);
-                printf("O quarto maior número digitado foi %d",A);
+                printf("\nO terceiro maior número digitado foi %d",C);
+                printf("\nO quarto maior número digitado foi %d",A);
             }
         }
     } else if (C>=A && C>=B && C>=D){
         printf("O maior número digitado foi %d",C);
-        if (B>=C && B>=D){
-            printf("O segundo maior número digitado foi %d",B);
-            if (C>=D){
-                printf("O terceiro maior número digitado foi %d",C);
-                printf("O quarto maior número digitado foi %d",D);
+        if (B>=A && B>=D){
+            printf("\nO segundo maior número digitado foi %d",B);
+            if (A>=D){
+                printf("\nO terceiro maior número digitado foi %d",A);
+                printf("\nO quarto maior número digitado foi %d",D);
             }else{
-                printf("O terceiro maior número digitado foi %d",D);
-                printf("O quarto maior número digitado foi %d",C);
+                printf("\nO terceiro maior número digitado foi %d",D);
+                printf("\nO quarto maior número digitado foi %d",A);
             }
-        } else if (C>=B && C>=D){
-            printf("O segundo maior número digitado foi %d",C);
+        } else if (A>=B && A>=D){
+            printf("\nO segundo maior número digitado foi %d",A);
             if (B>=D){
-                printf("O terceiro maior número digitado foi %d",B);
-                printf("O quarto maior número digitado foi %d",D);
+                printf("\nO terceiro maior número digitado foi %d",B);
+                printf("\nO quarto maior número digitado foi %d",D);
             }else{
-                printf("O terceiro maior número digitado foi %d",D);
-                printf("O quarto maior número digitado foi %d",B);
+                printf("\nO terceiro maior número digitado foi %d",D);
+                printf("\nO quarto maior número digitado foi %d",B);
             }
         } else{
-            printf("O segundo maior número digitado foi %d",D);
-            if (C>=B){
-                printf("O terceiro maior número digitado foi %d",C);
-                printf("O quarto maior número digitado foi %d",B);
+            printf("\nO segundo maior número digitado foi %d",D);
+            if (A>=B){
+                printf("\nO terceiro maior número digitado foi %d",A);
+                printf("\nO quarto maior número digitado foi %d",B);
             }else{
-                printf("O terceiro maior número digitado foi %d",B);
-                printf("O quarto maior número digitado foi %d",C);
+                printf("\nO terceiro maior número digitado foi %d",B);
+                printf("\nO quarto maior número digitado foi %d",A);
             }
         }
     } else{
         printf("O maior número digitado foi %d",D);
-        if (B>=C && B>=D){
-            printf("O segundo maior número digitado foi %d",B);
-            if (C>=D){
-                printf("O terceiro maior número digitado foi %d",C);
-                printf("O quarto maior número digitado foi %d",D);
+        if (B>=C && B>=A){
+            printf("\nO segundo maior número digitado foi %d",B);
+            if (A>=D){
+                printf("\nO terceiro maior número digitado foi %d",A);
+                printf("\nO quarto maior número digitado foi %d",D);
             }else{
-                printf("O terceiro maior número digitado foi %d",D);
-                printf("O quarto maior número digitado foi %d",C);
+                printf("\nO terceiro maior número digitado foi %d",D);
+                printf("\nO quarto maior número digitado foi %d",A);
             }
-        } else if (C>=B && C>=D){
-            printf("O segundo maior número digitado foi %d",C);
-            if (B>=D){
-                printf("O terceiro maior número digitado foi %d",B);
-                printf("O quarto maior número digitado foi %d",D);
+        } else if (C>=B && C>=A){
+            printf("\nO segundo maior número digitado foi %d",C);
+            if (A>=B){
+                printf("\nO terceiro maior número digitado foi %d",A);
+                printf("\nO quarto maior número digitado foi %d",B);
             }else{
-                printf("O terceiro maior número digitado foi %d",D);
-                printf("O quarto maior número digitado foi %d",B);
+                printf("\nO terceiro maior número digitado foi %d",B);
+                printf("\nO quarto maior número digitado foi %d",A);
             }
         } else{
-            printf("O segundo maior número digitado foi %d",D);
+            printf("\nO segundo maior número digitado foi %d",A);
             if (C>=B){
-                printf("O terceiro maior número digitado foi %d",C);
+                printf("\nO terceiro maior número digitado foi %d",C);
                 printf("O quarto maior número digitado foi %d",B);
             }else{
-                printf("O terceiro maior número digitado foi %d",B);
-                printf("O quarto maior número digitado foi %d",C);
+                printf("\nO terceiro maior número digitado foi %d",B);
+                printf("\nO quarto maior número digitado foi %d",C);
             }
         }
     }
